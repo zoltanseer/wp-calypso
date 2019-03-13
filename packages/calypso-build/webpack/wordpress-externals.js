d18661f5123b1b8e68c5d1be942b6dd6bd460b97
Alda Vigdís Skarphéðinsdóttir <alda.vigdis@a8c.com>
2019-03-13 19:53:30 +0100

    Extr. WP Extrn.

---
+++ packages/calypso-build/webpack/wordpress-externals.js
@@ -0,0 +1,29 @@
+/**
+ * Converts @wordpress require into window reference
+ *
+ * Note this isn't the same as camel case because of the
+ * way that numbers don't trigger the capitalized next letter
+ *
+ * @example
+ * wordpressRequire( '@wordpress/api-fetch' ) = 'wp.apiFetch'
+ * wordpressRequire( '@wordpress/i18n' ) = 'wp.i18n'
+ *
+ * @param {string} request import name
+ * @return {string} global variable reference for import
+ */
+const wordpressRequire = request => {
+	// @wordpress/components -> [ @wordpress, components ]
+	const [ , name ] = request.split( '/' );
+
+	// components -> wp.components
+	return `wp.${ name.replace( /-([a-z])/g, ( match, letter ) => letter.toUpperCase() ) }`;
+};
+
+const wordpressExternals = ( context, request, callback ) =>
+	/^@wordpress\//.test( request )
+		? callback( null, `root ${ wordpressRequire( request ) }` )
+		: callback();
+
+module.exports = () => ( {
+	wordpressExternals: wordpressExternals,
+} );
