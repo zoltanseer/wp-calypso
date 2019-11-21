67f7535ed2c6ffb55bb26ea1b36195d2a0739028
Jon Surrell <jon.surrell@automattic.com>
2019-11-21 23:17:07 +0100

    Type action-log

--- client/state/action-log/index.ts
+++ client/state/action-log/index.ts
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+import { AnyAction, StoreEnhancer } from 'redux';
+import { Falsy } from 'utility-types';
+
+/**
  * Action Log Redux store enhancer
  *
  * Inject at the bottom of the `createStore` enhancer
@@ -10,14 +16,22 @@
  * chain, passing straight through.
  */
 
-const state = {
+type WatchPredicate = ( a: AnyAction ) => boolean;
+interface State {
+	actionHistory: AnyAction[];
+	shouldRecordActions: boolean;
+	historySize: number;
+	watchPredicate: null | ( ( a: AnyAction ) => boolean );
+}
+const state: State = {
 	actionHistory: [],
 	shouldRecordActions: true,
 	historySize: 100,
 	watchPredicate: null,
 };
 
-export const queryToPredicate = query => {
+type Query = RegExp | string | WatchPredicate;
+export function queryToPredicate( query: Query ): WatchPredicate {
 	if ( query instanceof RegExp ) {
 		return ( { type } ) => query.test( type );
 	}
@@ -29,16 +43,17 @@ export const queryToPredicate = query => {
 	}
 
 	throw new TypeError( 'provide string or RegExp matching `action.type` or a predicate function' );
-};
+}
 
 const actionLog = {
 	clear: () => void ( state.actionHistory = [] ),
-	filter: query => state.actionHistory.filter( queryToPredicate( query ) ),
-	setSize: size => void ( state.historySize = size ),
+	filter: ( query: Query ) => state.actionHistory.filter( queryToPredicate( query ) ),
+	setSize: ( size: number ) => void ( state.historySize = size ),
 	start: () => void ( state.shouldRecordActions = true ),
 	stop: () => void ( state.shouldRecordActions = false ),
 	unwatch: () => void ( state.watchPredicate = null ),
-	watch: query => void ( state.watchPredicate = query ? queryToPredicate( query ) : null ),
+	watch: ( query: Query | Falsy ) =>
+		void ( state.watchPredicate = query ? queryToPredicate( query ) : null ),
 };
 
 Object.defineProperty( actionLog, 'history', {
@@ -46,7 +61,7 @@ Object.defineProperty( actionLog, 'history', {
 	get: () => state.actionHistory,
 } );
 
-const recordAction = action => {
+const recordAction = ( action: AnyAction ) => {
 	const { actionHistory, historySize } = state;
 
 	const thunkDescription = 'function' === typeof action ? { type: 'thunk (hidden)' } : {};
@@ -67,7 +82,7 @@ const recordAction = action => {
 	}
 };
 
-export const actionLogger = next => ( ...args ) => {
+export const actionLogger: StoreEnhancer = next => ( ...args ) => {
 	const store = next( ...args );
 
 	if ( 'undefined' === typeof window ) {
