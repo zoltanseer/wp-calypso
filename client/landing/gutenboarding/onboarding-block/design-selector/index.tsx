e566f5e1b3719cd39ee6c8555228694de55b5654
Yan Sern <yan.sern@automattic.com>
2020-02-10 11:40:01 +0100

    Clicking outside of the page selector should not hide the page selector to allow tabbing in & out to work.

--- client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
+++ client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
@@ -159,7 +159,7 @@ const DesignSelector: FunctionComponent< Props > = ( { showPageSelector = false 
 					history.push( Step.DesignSelection );
 				} }
 				aria-labelledby="page-layout-selector__title"
-				hideOnClickOutside
+				hideOnClickOutside={ false }
 				hideOnEsc
 			>
 				<animated.div
