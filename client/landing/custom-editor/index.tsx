c5b92617a9d6b53a928adc22859c019ee19b13d4
Bernie Reiter <ockham@raz.or.at>
2020-02-12 15:33:12 +0600

    Use edit-site

--- client/landing/custom-editor/index.tsx
+++ client/landing/custom-editor/index.tsx
@@ -1,38 +1,13 @@
 /**
  * External dependencies
  */
-import '@automattic/calypso-polyfills';
-import React from 'react';
-import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import config from '../../config';
-
-/**
- * Internal dependencies
- */
-import CustomEditor from './editor';
-// import { setupWpDataDebug } from './devtools'; // Currently only available in Gutenboarding
-import accessibleFocus from 'lib/accessible-focus';
-/**
- * Style dependencies
- */
-import 'assets/stylesheets/gutenboarding.scss';
-import 'components/environment-badge/style.scss';
+import { initialize } from '@wordpress/edit-site';
 
 window.AppBoot = () => {
 	if ( ! config.isEnabled( 'custom-editor' ) ) {
 		window.location.href = '/';
 	} else {
-		// setupWpDataDebug();
-
-		// Add accessible-focus listener.
-		accessibleFocus();
-
-		ReactDom.render(
-			<BrowserRouter basename="custom-editor">
-				<CustomEditor />
-			</BrowserRouter>,
-			document.getElementById( 'wpcom' )
-		);
+		initialize( 'wpcom', {} );
 	}
 };
